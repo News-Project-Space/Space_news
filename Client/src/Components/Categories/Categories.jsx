6a4e46b54aa9f07d790221a298174a17d3f0a79e
l2vrns <lorans0alkhalailah@gmail.com>
2025-03-12 23:48:37 +0300

    done

--- Client/src/Components/Categories/Categories.jsx
+++ Client/src/Components/Categories/Categories.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function Categories() {
   return (
-    <div>
-      
-    </div>
+    <div>Categories</div>
   )
 }
 
---
+++ Client/src/Components/NewsArticleCreation/NewsArticleCreation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewsArticleCreation() {
+  return (
+    <div>NewsArticleCreation</div>
+  )
+}
+
+export default NewsArticleCreation
